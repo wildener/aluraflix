ec44f54f38a03701c1519d0d4b7b181d02d125ac
wildener <wildener@users.noreply.github.com>
2021-07-23 14:04:16 -0300

    Semana 1 - Validação da URL.

--- backflix/models.py
+++ backflix/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Video(models.Model):
-    titulo = models.CharField(max_length=100, null=False)
+    titulo = models.CharField(max_length=100, null=False, unique=True)
     descricao = models.CharField(max_length=500, null=False)
-    url = models.CharField(max_length=100, null=False)
+    url = models.CharField(max_length=100, null=False, unique=True)
 
     def __str__(self):
         return self.titulo
--- backflix/serializer.py
+++ backflix/serializer.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from backflix.models import Video
+import validators
 
 
 class VideoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Video
         fields = ['id', 'titulo', 'descricao', 'url']
+
+    def validate_url(self, url):
+        if not validators.url(url):
+            raise serializers.ValidationError('URL inválida')
+        return url
